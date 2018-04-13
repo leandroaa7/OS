c6f3b4e4cbb259bae540266a4d915e47ebde46ad
leandroaa7 <leandroaalexandre@gmail.com>
2018-04-13 20:02:28 -0300

    criacao de metodos e tratamento de erros

--- Scheduling/project1/project1.py
+++ Scheduling/project1/project1.py
@@ -1,4 +1,4 @@
-
+myprocesses = []
 def mensagem():
     print(" ")
     print("Atenção! o formato do processo deve ser:")
@@ -7,8 +7,22 @@ def mensagem():
     print(" 3  20  3")
     print(" ")
 def main():
-    mensagem()
-    process =  input(" digite um processo")
-    asd = tuple(states.split (' '))
-    print(asd[0])
+    try:
+        mensagem()
+        states =  input(" digite um processo")
+        asd = tuple(states.split (' '))
+        print(len(states))
+        if(len(states) != 5): # 5 pois conta os 3 processos + espaços
+            raise Exception("Entrada Incorreta")
+        
+    except Exception as error:
+        print(error)
+def fifo( process ):
+    pass
+def sjf( process ):
+    pass
+def rr( process ):
+    pass
+def prior( process ):
+    pass
 main()
