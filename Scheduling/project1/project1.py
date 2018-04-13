cfcd7b0c61bc09ef51fc757f57b6da2e740b49e2
leandroaa7 <leandroaalexandre@gmail.com>
2018-04-12 21:10:39 -0300

    implementação de mensagem e entrada de dados correta

--- Scheduling/project1/project1.py
+++ Scheduling/project1/project1.py
@@ -1,3 +1,14 @@
+
+def mensagem():
+    print(" ")
+    print("Atenção! o formato do processo deve ser:")
+    print(" tempos de chegada <espaço> duração <espaço>  prioridade")
+    print("exemplo")
+    print(" 3  20  3")
+    print(" ")
 def main():
-    print("helloo");
+    mensagem()
+    process =  input(" digite um processo")
+    asd = tuple(states.split (' '))
+    print(asd[0])
 main()
